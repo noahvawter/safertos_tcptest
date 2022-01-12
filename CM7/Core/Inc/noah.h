35ae65d31468e8d22a2fc17c9425aeb420031b70
Noah Vawter <nvawter@vicarioussurgical.com>
2022-01-12 10:33:19 -0500

    first commit

---
+++ CM7/Core/Inc/noah.h
@@ -0,0 +1,15 @@
+/*
+ * noah.h
+ *
+ *  Created on: Jan 12, 2022
+ *      Author: nvawter
+ */
+
+#ifndef INC_NOAH_H_
+#define INC_NOAH_H_
+
+
+void noah_hello( void );
+
+
+#endif /* INC_NOAH_H_ */
---
+++ CM7/Core/Src/noah.c
@@ -0,0 +1,17 @@
+/*
+ * noah.c
+ *
+ *  Created on: Jan 12, 2022
+ *      Author: nvawter
+ */
+
+#include <stdint.h>
+
+void noah_hello( void )
+{
+
+	static int32_t x=0;
+
+	x++;
+
+}
